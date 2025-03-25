963a67abf256d22670161301cb5b5229c4a32ace
Simonas Radžius <47453208+Reflexxion19@users.noreply.github.com>
2025-03-25 18:38:50 +0200

    Minor fixes

--- php/employee/loans.php
+++ php/employee/loans.php
@@ -21,7 +21,7 @@ if($_SESSION['role'] != 'employee'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Analizė</title>
+    <title>Paskolos</title>
     <link rel="stylesheet" href="../../css/header.css">
     <link rel="stylesheet" href="../../css/style.css">
     <script src="../../js/header.js"></script>
